0cd798c8f91ec30304af1b510783d03afa159135
Hanry <hanryjosearaujo@gmail.com>
2024-04-29 17:06:02 -0300

    Tipos de variaveis

---
+++ primeiro-projeto-java/src/edu/hanry/semana1prog/TiposVariaveis.java
@@ -0,0 +1,17 @@
+package edu.hanry.semana1prog;
+
+public class TiposVariaveis {
+    public static void main(String[] args) {
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        int numero = 5;
+
+        numero = 10;
+        
+        System.out.println(numero);
+
+        final double VALOR_DE_PI = 3.14;
+    }
+}
