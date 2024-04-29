abb02fe95aea1f9bd25f6f2e62a92fc1c29abc2a
Hanry <hanryjosearaujo@gmail.com>
2024-04-29 15:36:23 -0300

    Primeiros métodos

--- primeiro-projeto-java/src/Aulas.java
+++ primeiro-projeto-java/src/Aulas.java
@@ -1,5 +1,16 @@
 public class Aulas {
     public static void main(String[] args) {
         
+        String primeiroNome = "Hanry";
+        String segundoNome = "José";
+
+        String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+        
+        System.out.println(nomeCompleto);
     }
+
+    public static String nomeCompleto(String primeiroNome, String segundoNome) {
+        return "Resultado do método " + primeiroNome.concat(" ").concat(segundoNome);
+    }
+
 }
