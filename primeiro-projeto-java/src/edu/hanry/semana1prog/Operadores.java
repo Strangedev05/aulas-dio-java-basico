0b2aedcd344a1b7c243b6e74b0b5181553b222bf
Hanry <hanryjosearaujo@gmail.com>
2024-04-30 15:52:13 -0300

    aula de operadores

---
+++ primeiro-projeto-java/src/edu/hanry/semana1prog/Operadores.java
@@ -0,0 +1,59 @@
+package edu.hanry.semana1prog;
+
+public class Operadores {
+    public static void main(String[] args) {
+       // int numero = 5;
+
+       // System.out.println(++ numero);
+
+       // int a, b;
+       // a = 5;
+       // b = 6;
+
+       // String resultado = a==b ? "verdadeiro" : "falso";
+
+       // System.out.println(resultado);
+
+       // String nome1 = "Hanry";
+       // String nome2 = "Hanry";
+
+       // System.out.println(nome1 == nome2);
+
+       // int n1 = 1;
+       // int n2 = 2;
+
+       // if(n1 == n2){
+       //     System.out.println("a nossa condição é verdadeira");
+       // }
+
+       // boolean simNao = n1 == n2;
+
+       // System.out.println("NúmeroUm é igual ao núemroDois? " + simNao);
+
+        //simNao = n1 != n2;
+
+       // System.out.println("NúmeroUm é diferente ao núemroDois? " + simNao);
+
+       // simNao = n1 > n2;
+
+       // System.out.println("NúmeroUm é maior ao núemroDois? " + simNao);
+
+        //simNao = n1 < n2;
+
+        //System.out.println("NúmeroUm é menor ao núemroDois? " + simNao);
+
+        boolean cond1 = true;
+
+        boolean cond2 = false;
+
+        if(cond1 && (7 > 4)){
+            System.out.println("as duas condinções são verdaddeiras");
+        }
+
+        if(cond1 || cond2){
+            System.out.println("uma das condinções é verdaddeira");
+        }
+
+        System.out.println("fim");
+    }
+}
