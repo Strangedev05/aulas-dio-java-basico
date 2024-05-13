a76d826e99829991806b6461cf797d8d63217ba4
Hanry <hanryjosearaujo@gmail.com>
2024-05-13 09:20:36 -0300

    Condiçoes encadeada

--- arquivos-de-aulas-java/src/edu/hanry/semana2prog/ResultadoEscolar.java
+++ arquivos-de-aulas-java/src/edu/hanry/semana2prog/ResultadoEscolar.java
@@ -3,10 +3,13 @@ package edu.hanry.semana2prog;
 public class ResultadoEscolar {
     public static void main(String[] args) {
         
-        int nota = 10;
+        int nota = 4;
 
         if(nota >= 7)
             System.out.println("Aprovado");
+
+        else if (nota > 5 && nota < 7)
+            System.out.println("Recuperação");
 
         else
             System.out.println("Reprovado");
