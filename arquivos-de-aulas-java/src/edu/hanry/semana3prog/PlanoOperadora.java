a9034e49c871266eb4fd14d44fa4fa459f76aa1a
Hanry <hanryjosearaujo@gmail.com>
2024-05-13 10:00:07 -0300

    Switch Case

---
+++ arquivos-de-aulas-java/src/edu/hanry/semana3prog/PlanoOperadora.java
@@ -0,0 +1,19 @@
+package edu.hanry.semana3prog;
+
+public class PlanoOperadora {
+    public static void main(String[] args) {
+        String plano = "T";
+        
+        switch (plano) {
+            case "T": {
+                System.out.println("5Gb YouTube");
+            }
+            case "M": {
+                System.out.println("Whats e Insta Grátis");
+            }
+            case "B": {
+                System.out.println("100 min de ligação");
+            }
+        }
+    }
+}
