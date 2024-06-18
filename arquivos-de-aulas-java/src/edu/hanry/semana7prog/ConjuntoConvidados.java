1a6c04f1f6b3c6e821ff7e4f7e689e192a6e5ffb
Hanry <hanryjosearaujo@gmail.com>
2024-06-18 09:19:51 -0300

    Operação Básica com Set

---
+++ arquivos-de-aulas-java/src/edu/hanry/semana7prog/ConjuntoConvidados.java
@@ -0,0 +1,51 @@
+package edu.hanry.semana7prog;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class ConjuntoConvidados {
+    private Set<Convidado> convidadoSet;
+
+    public ConjuntoConvidados() {
+        this.convidadoSet = new HashSet<>();
+    }
+
+    public void adicionarConvidado(String nome, int codConvite) {
+        convidadoSet.add(new Convidado(nome, codConvite));
+    }
+
+    public void removerCodConvidado(int codConvite) {
+        Convidado removerCodigo = null;
+        for(Convidado c : convidadoSet) {
+            if(c.getCodConvite() == codConvite) {
+                removerCodigo = c;
+            }
+        }
+        convidadoSet.remove(removerCodigo);
+    }
+
+    public int contarConvidados() {
+        return convidadoSet.size();
+    }
+
+    public void exibirConvidados() {
+        System.out.println(convidadoSet);
+    }
+
+    public static void main(String[] args) {
+        ConjuntoConvidados conjuntoConvidados = new ConjuntoConvidados();
+        //System.out.println("Existem " + conjuntoConvidados.contarConvidados() + " convidado(s) dentro do Set de Convidados");
+
+        conjuntoConvidados.adicionarConvidado("Convidado 1", 1234);
+        conjuntoConvidados.adicionarConvidado("Convidado 2", 1235);
+        conjuntoConvidados.adicionarConvidado("Convidado 3", 1235);
+        conjuntoConvidados.adicionarConvidado("Convidado 4", 1236);
+
+        System.out.println("Existem " + conjuntoConvidados.contarConvidados() + " convidado(s) dentro do Set de Convidados");
+
+        conjuntoConvidados.removerCodConvidado(1234);
+        System.out.println("Existem " + conjuntoConvidados.contarConvidados() + " convidados(s) dentro do Set de Convidados");
+
+        conjuntoConvidados.exibirConvidados();
+    }
+}
---
+++ arquivos-de-aulas-java/src/edu/hanry/semana7prog/Convidado.java
@@ -0,0 +1,48 @@
+package edu.hanry.semana7prog;
+
+public class Convidado {
+    private String nome;
+    private int codConvite;
+    
+    public Convidado(String nome, int codConvite) {
+        this.nome = nome;
+        this.codConvite = codConvite;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getCodConvite() {
+        return codConvite;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + codConvite;
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Convidado other = (Convidado) obj;
+        if (codConvite != other.codConvite)
+            return false;
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Convidado [nome=" + nome + ", codConvite=" + codConvite + "]";
+    }
+
+    
+}
